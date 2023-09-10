a75929223268b831c719b4f383580b622d1cf7b5
Harrison <hhathersage@gmail.com>
2023-09-10 14:09:26 +0300

    tag jhui/fidget to legacy.

--- init.lua
+++ init.lua
@@ -87,7 +87,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      { 'j-hui/fidget.nvim',       tag = "legacy", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -112,15 +112,9 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-<<<<<<< HEAD
   { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
-=======
-  { 'folke/which-key.nvim',          opts = {} },
-  {
-    -- Adds git releated signs to the gutter, as well as utilities for managing changes
->>>>>>> 4629455 (autoinstall language servers.)
     'lewis6991/gitsigns.nvim',
     opts = {
       -- See `:help gitsigns.txt`
@@ -430,13 +424,8 @@ vim.defer_fn(function()
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'elixir', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
-<<<<<<< HEAD
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
-=======
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = true,
->>>>>>> 4629455 (autoinstall language servers.)
 
     highlight = { enable = true },
     indent = { enable = true },
