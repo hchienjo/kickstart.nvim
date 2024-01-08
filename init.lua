8d04c398d5ad8775c6a1247f5c368afbbc9121be
Mohamad Alamin Yassin <ameen.yaseen.98@gmail.com>
2024-01-08 20:17:01 +0300

    Add Missing Fields to Treesitter Config to Resolve Warnings (#582)
    This commit introduces three additional fields - `sync_install`, `ignore_install`, and `modules` - to the Treesitter configuration. This update is aimed at resolving warnings that were previously displayed, potentially causing confusion or frustration for new users of Neovim. By explicitly defining these fields, the configuration aligns better with the latest `nvim-treesitter` requirements.

--- init.lua
+++ init.lua
@@ -474,10 +474,8 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'elixir', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
-
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
-
     highlight = { enable = true },
     indent = { enable = true },
     incremental_selection = {
