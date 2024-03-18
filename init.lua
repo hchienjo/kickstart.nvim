558c2faccd0db8031e4bc8d3caba9b6658d48550
Shane Crowley <66971213+edibotopic@users.noreply.github.com>
2024-03-18 13:57:48 +0000

    assign table to filetype in lua_ls config comment (#770)

--- init.lua
+++ init.lua
@@ -552,7 +552,7 @@ require('lazy').setup({
 
         lua_ls = {
           -- cmd = {...},
-          -- filetypes { ...},
+          -- filetypes = { ...},
           -- capabilities = {},
           settings = {
             Lua = {
